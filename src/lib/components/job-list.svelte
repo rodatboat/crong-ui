f672d9a2c111d4d7510edf8e0a3a910ff6bf1799
rodatboat <ronaldo3055@gmail.com>
2026-07-05 01:43:06 -0500

    job list

--- src/lib/components/job-list.svelte
+++ src/lib/components/job-list.svelte
@@ -1,9 +1,17 @@
 <script lang="ts">
     import * as Card from "$lib/components/ui/card";
+    import { Button } from "$lib/components/ui/button";
     import { JOB_METHOD_MAP } from "$lib/const";
     import type { Job } from "$lib/types";
+    import PlayIcon from "@lucide/svelte/icons/play";
+    import EditIcon from "@lucide/svelte/icons/edit";
+    import HistoryIcon from "@lucide/svelte/icons/history";
+    import LoaderIcon from "@lucide/svelte/icons/loader-circle";
+    import { triggerJob } from "$lib/services/jobs";
+    // import { resolve } from "$app/paths";
 
     let { jobs }: { jobs: Job[] } = $props();
+    let runningJob: boolean = $state(false);
 </script>
 
 <section class="items-center gap-6 pb-8 pt-6 md:py-10">
@@ -27,8 +35,31 @@
                     </Card.Description>
                 </Card.Header>
 
-                <Card.Footer>
-                    <Card.Action>Apply</Card.Action>
+                <Card.Footer class="flex justify-start md:justify-end gap-2 p-2">
+                    <Button variant="ghost" class="md:order-1 order-3 w-min hover:cursor-pointer"
+                        ><HistoryIcon class="size-3" /> History</Button
+                    >
+                    <Button
+                        variant="secondary"
+                        class="w-min hover:cursor-pointer order-2"
+                        ><EditIcon class="size-3" /> Edit</Button
+                    >
+                    <Button
+                        variant="default"
+                        class="md:order-3 order-1 w-16 hover:cursor-pointer"
+                        onclick={async () => {
+                            runningJob = true;
+                            await triggerJob(job.id);
+                            runningJob = false;
+                        }}
+                    >
+                        {#if runningJob}
+                            <LoaderIcon class="size-3 animate-spin" />
+                        {:else}
+                            <PlayIcon class="size-3" />
+                            Run
+                        {/if}
+                    </Button>
                 </Card.Footer>
             </Card.Root>
         {/each}
